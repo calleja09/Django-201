714d248c6549f46f419691982ae050bf2a85eb1b
Arvin Calleja <callejaarvin09@gmail.com>
2024-01-23 11:28:37 +0800

    adding user profiles

--- til/settings.py
+++ til/settings.py
@@ -18,6 +18,9 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = "/media/"
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -42,9 +45,11 @@ INSTALLED_APPS = [
     'django.contrib.sites',
 
     'feed',
+    'profiles',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
